660c3a243cb41ce0af6ff38afa333f3b0dae17bc
Camille TOURON <camille.touron.pro@gmail.com>
2022-11-07 09:39:23 +0100

    Initialisation back - spring hibernates.

---
+++ src/main/java/com/greenswap/greenswap/GreenswapApplication.java
@@ -0,0 +1,13 @@
+package com.greenswap.greenswap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenswapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenswapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greenswap/greenswap/GreenswapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenswap.greenswap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenswapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
